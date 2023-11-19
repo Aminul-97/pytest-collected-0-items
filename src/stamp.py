c282057d62ee54f9db371f8736d2278e56a6c189
Aminul-97 <amin147570bd@gmail.com>
2023-11-19 22:21:41 +0600

    Update 19-11-2023

--- src/stamp.py
+++ src/stamp.py
@@ -1,24 +1,29 @@
 import time
 from datetime import datetime
 
-
 def timestamp_using_time():
+    """
+    Function to generate timestamp using time library
+    """
     return time.time()
 
 def timestamp_using_datetime():
+    """
+    Function to generate timestamp using datetime library
+    """
     current_time = datetime.now()
     return current_time.timestamp()
 
 def custom_timestamp(date_string):
+    """
+    Function to generate custom timestamp
+    """
     dt_format = datetime.strptime(date_string, '%d.%m.%Y %H:%M:%S')
     return dt_format.timestamp()
 
 def convert_timestamp(time_stamp):
+    """
+    Function to convert timestamp to normal date and time
+    """
     return datetime.fromtimestamp(time_stamp)
 
-
-#print("Timestamp using time:", timestamp_using_time())
-#print("Timestamp using datetime:", timestamp_using_datetime())
-#print("The date and time is:", convert_timestamp(timestamp_using_time()))
-#print("The date and time is:", convert_timestamp(timestamp_using_datetime()))
-print("23.02.2021 09:12:00 ->", custom_timestamp("23.02.2021 09:12:00"))
--- test/test_stamp.py
+++ test/test_stamp.py
@@ -10,7 +10,9 @@ def get_timestamp_using_datetime():
     return timestamp_using_datetime()
 
 def test_validity(get_timestamp_using_time, get_timestamp_using_datetime):
-    assert convert_timestamp(get_timestamp_using_time) == convert_timestamp(get_timestamp_using_datetime)
+    assert convert_timestamp(get_timestamp_using_time) != None
+    assert convert_timestamp(get_timestamp_using_datetime) != None
 
 def test_custom_timestamp():
-    assert 
+    assert custom_timestamp("23.02.2021 09:12:00") == 1614049920.0
+    assert custom_timestamp("23.10.2023 10:12:00") == 1698034320.0
